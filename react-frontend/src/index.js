b4a293d433fb14bc666c90fba94549b2e37a8aec
Cameron McGiffert <rmcgiffe@calpoly.edu>
2022-05-08 00:42:19 -0700

    Deleted unused files

--- react-frontend/src/index.js
+++ react-frontend/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import CanvasDraw from "react-canvas-draw";
+//import CanvasDraw from "react-canvas-draw";
 //import MyApp from './MyApp'
 import './index.css'
 import App from './App';
