5f0b3c1993503fce4000bb0c797cc59434b60225
seena18 <seena101.sa@gmail.com>
2022-05-13 13:52:00 -0700

    pretty work

--- expressjs-backend/backend.js
+++ expressjs-backend/backend.js
@@ -1,5 +1,5 @@
-const cors = require('cors');
-const express = require('express');
+const cors = require("cors");
+const express = require("express");
 const app = express();
 const port = 5000;
 
@@ -15,70 +15,70 @@ const WIDTH = 40;
 
 const pixels = { pixelList: initializeList(HEIGHT, WIDTH) };
 function initializeList(height, width) {
-    l = [];
-    for (let x = 0; x < width; x++) {
-        for (let y = 0; y < height; y++) {
-            l.push({
-                color: "#fff",
-                x: x,
-                y: y
-            });
-        }
+  l = [];
+  for (let x = 0; x < width; x++) {
+    for (let y = 0; y < height; y++) {
+      l.push({
+        color: "#fff",
+        x: x,
+        y: y,
+      });
     }
-    return l;
+  }
+  return l;
 }
 
-app.get('/pixels/', (req, res) => {
-    res.send(pixels);
+app.get("/pixels/", (req, res) => {
+  res.send(pixels);
 });
 
-app.get('/pixels/:id', (req, res) => {
-    const id = req.params['id'];
-    let result = findPixelById(id);
-    if (result === undefined || result.length == 0)
-        res.status(404).send('Resource not found.');
-    else {
-        result = { pixelList: result };
-        res.send(result);
-    }
+app.get("/pixels/:id", (req, res) => {
+  const id = req.params["id"];
+  let result = findPixelById(id);
+  if (result === undefined || result.length == 0)
+    res.status(404).send("Resource not found.");
+  else {
+    result = { pixelList: result };
+    res.send(result);
+  }
 });
 
 function findPixelById(id) {
-    return pixels['pixelList'].find((pixel) => pixel['id'] === id);
+  return pixels["pixelList"].find((pixel) => pixel["id"] === id);
 }
 
 //pixels?username=
-app.get('/pixels', (req, res) => {
-    const username = req.query.username;
-    if (username != undefined) {
-        let result = findPixelsPlacedBy(username);
-        result = { pixelList: result };
-        res.send(result);
-    }
-    else {
-        res.send(pixels);
-    }
+app.get("/pixels", (req, res) => {
+  const username = req.query.username;
+  if (username != undefined) {
+    let result = findPixelsPlacedBy(username);
+    result = { pixelList: result };
+    res.send(result);
+  } else {
+    res.send(pixels);
+  }
 });
 
 const findPixelsPlacedBy = (name) => {
-    return pixels['pixelList'].filter((pixel) => pixel['username'] === name);
-}
+  return pixels["pixelList"].filter((pixel) => pixel["username"] === name);
+};
 
-app.post('/pixels', (req, res) => {
-    const p = req.body;
-    let index = pixels['pixelList'].findIndex(pixel =>
-        pixel['x'] === p['x'] && pixel['y'] === p['y']);
-    pixels['pixelList'][index]['color'] = p['color'];
+app.post("/pixels", (req, res) => {
+  const p = req.body;
+  let index = pixels["pixelList"].findIndex(
+    (pixel) => pixel["x"] === p["x"] && pixel["y"] === p["y"]
+  );
+  pixels["pixelList"][index]["color"] = p["color"];
 
-    res.status(201).end();
+  res.status(201).end();
 });
 
 // Clears out all pixel data
-app.delete('/pixels/', (req, res) => {
-    pixels['pixelList'] = initializeList(HEIGHT, WIDTH);
-    res.status(204).end();
-})
+app.delete("/pixels/", (req, res) => {
+  pixels["pixelList"] = initializeList(HEIGHT, WIDTH);
+  res.status(204).end();
+});
 
 app.listen(port, () => {
-    console.log(`Pixlerr listening at http://localhost:${port}`);
+  console.log(`Pixlerr listening at http://localhost:${port}`);
 });
