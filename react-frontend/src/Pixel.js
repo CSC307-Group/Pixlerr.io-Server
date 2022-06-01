8d6575049c0e77f0bafd4558e118e510229840e6
reedmarohn <reedmarohn@yahoo.com>
2022-06-01 14:26:25 -0700

    Working commit with merged user Schema changes

--- react-frontend/src/Pixel.js
+++ react-frontend/src/Pixel.js
@@ -11,8 +11,6 @@ export default function Pixel(props) {
         <div
             className="pixel"
             onClick={applyColor}
-            onMouseEnter={changeColorOnHover}
-            onMouseLeave={resetColor}
             style={{ backgroundColor: pixel['color'] }}
         ></div>
     );
