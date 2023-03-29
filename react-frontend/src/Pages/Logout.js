88724772df75b2927f2b01e2caf741e0b2e7df2b
cwmcgiffert <91047092+rcamwm@users.noreply.github.com>
2023-03-29 13:19:22 -0700

    Fixed logout issue

--- react-frontend/src/Pages/Logout.js
+++ react-frontend/src/Pages/Logout.js
@@ -1,39 +1,16 @@
-import { useEffect } from "react";
-import axios from "axios";
-import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
-const userhost = 'http://localhost:5000/Logout';
-const userhost1 = 'http://localhost:5000/users';
-export default function Signout() {
-    let nav = useNavigate();
+const Logout = ({ user }) => {
+  const logout = () => {
+    window.open("http://localhost:5000/logout", "_self");
+  };
+  return (
+    <div className="navbar">
+      <li className="listItem" onClick={logout}>
+        Logout
+      </li>
+    </div>
+  );
+};
 
-    useEffect(() => {
-        getUser();
-    }, []);
-
-    const getUser = () => {
-        axios({
-            method: "DELETE",
-            withCredentials: true,
-            url: userhost,
-        }).then((res) => {
-            console.log("test");
-
-        });
-    };
-    
-    useEffect(() => {
-        fillUserStates();
-    }, []);
-
-    const fillUserStates = () => {
-        axios({
-            method: "GET",
-            withCredentials: true,
-            url: userhost1,
-        }).then((res) => {
-            console.log(res.data)
-            nav('/');
-        });
-    };
-}
+export default Logout;
